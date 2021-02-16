dd8a21fd7c32c5825e90d8edf1384f9b64757d3e
Jakub Starostecki <64737924+PouekDEV@users.noreply.github.com>
2021-02-16 10:35:00 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2
 import pytesseract
 import os
-pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract.exe'
 t = input("Drag and drop image here: ")
 t = str(t).replace('"','')
 img = cv2.imread(t)
